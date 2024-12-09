76b73744454b71da3e34f982aa58a568b530275d
Barrytruth <barrytruth@hotmail.com>
2024-12-09 16:59:34 +0800

    update code for debug

--- dataCrawl/datafrom/seatMap.py
+++ dataCrawl/datafrom/seatMap.py
@@ -13,6 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from io import BytesIO
